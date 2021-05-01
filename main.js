e19b9049b5a1b45e63d873ac5a6c197b4e1bf4c1
rzv09 <rzv090701@gmail.com>
2021-05-01 13:50:54 -0400

    added functionality to convert number to a string

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // get command line args
 var args = process.argv.slice(2);
 // console.log('args: ', args);
-
+var result = "";
 var nums = {
     "0" : "Zero",
     "1" : "One",
@@ -14,3 +14,25 @@ var nums = {
     "8" : "Eight",
     "9" : "Nine"
 };
+
+for (let i=0; i<args.length; i++) {
+    let num = parseInt(args[i]);
+    let stringNum = "";
+    let minus = "";
+    // if a number is negative
+    if (num < 0) {
+        minus = "Minus";
+        num = Math.abs(num);
+    }
+    if (num == 0) {
+        stringNum = nums[num];
+    }
+    while (num != 0) {
+        stringNum = nums[num%10] + stringNum;
+        num = Math.floor(num/10);
+    }
+    stringNum = minus + stringNum;
+    i == args.length-1 ? result += stringNum : result += stringNum + ",";
+
+}
+console.log(result)
