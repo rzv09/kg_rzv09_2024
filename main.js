6ffe2aba502739de2c1f5a4a351aec7615fb9ac7
Raman Zatsarenko <rz4983@rit.edu>
2021-05-01 12:44:57 -0400

    Started working, added nums object.

---
+++ main.js
@@ -0,0 +1,16 @@
+// get command line args
+var args = process.argv.slice(2);
+// console.log('args: ', args);
+
+var nums = {
+    "0" : "Zero",
+    "1" : "One",
+    "2" : "Two",
+    "3" : "Three",
+    "4" : "Four",
+    "5" : "Five",
+    "6" : "Six",
+    "7" : "Seven",
+    "8" : "Eight",
+    "9" : "Nine"
+};
